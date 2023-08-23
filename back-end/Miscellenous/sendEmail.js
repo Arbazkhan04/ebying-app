c2497148dffe894c6c645bf4eb402dabff159b00
Arbaz <arbaz582021@gmail.com>
2023-08-23 05:41:03 +0500

    add

--- back-end/Miscellenous/sendEmail.js
+++ back-end/Miscellenous/sendEmail.js
@@ -2,6 +2,7 @@ const nodemailer = require("nodemailer");
 
 const senEmail = async (payLoad) => {
   try {
+    console.log(payLoad.email)
     // Generate test SMTP service account from ethereal.email
     // Only needed if you don't have a real mail account for testing
     // create reusable transporter object using the default SMTP transport
@@ -32,6 +33,19 @@ const senEmail = async (payLoad) => {
     // Send Email 
 
     const _SendEmail = await _SmtpService.sendMail(_EmailObject);
+    
+       // Sending confirmation email to the user
+       const confirmationEmailObject = {
+        from: 'arbaz582023@gmail.com', // sender address (your email)
+        to: payLoad.email, // user's email
+        subject: 'Confirmation: We received your email',
+        html: `<p>Dear ${payLoad.name},</p>
+        <p>We have received your inquiry. Our team will get back to you shortly.</p>
+        <p>Thank you for contacting us!</p>`,
+      };
+  
+      const consfirmationEmailResponse = await _SmtpService.sendMail(confirmationEmailObject); // Sending con
+      console.log(consfirmationEmailResponse.response)
     return {
       Message: `Email has sent successfuly one of our team member will contact you shortly`,
       Data: true,
